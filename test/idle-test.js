c290192a86f94d54c6b6d71fdecc52b2974d87ba
Kevin Ou <therealkevinou@yahoo.com>
2017-10-16 21:49:31 -0500

    removed duplicate test and renamed test file

--- test/idle-test.js
+++ test/idle-test.js
@@ -5,10 +5,8 @@ const timeUntilLevelupString = require('../src/TimeUtil');
 describe("calculate time to level", () => { 
   it("initial level up time is 5 minutes", () => { 
     assert.equal(Idle.prototype.calculateTimeToLevel(1), 300); 
-    assert.equal(timeUntilLevelupString(300), "5 minutes, 0 seconds"); 
   }) 
   it("second level up time is slightly more than 5 minutes", () => { 
     assert.equal(Idle.prototype.calculateTimeToLevel(2), 300 * 1.16); 
-    assert.equal(timeUntilLevelupString(300 * 1.16), "5 minutes, 48 seconds"); 
   }) 
 });
